5d040013a65ece4bc74ec7e4b105c45d10c51a55
rrenat358 <rrenat358@gmail.com>
2023-06-30 16:24:28 +0300

    1624 temp

--- ReMarket-02-08/cart-service/src/main/java/ru/rrenat358/cart/exceptions/GlobalExceptionHandler.java
+++ ReMarket-02-08/cart-service/src/main/java/ru/rrenat358/cart/exceptions/GlobalExceptionHandler.java
@@ -12,8 +12,14 @@ import ru.rrenat358.api.exceptions.ResourceNotFoundException;
 @Slf4j
 public class GlobalExceptionHandler {
     @ExceptionHandler
-    public ResponseEntity<AppError> catchResourceNotFoundException(ResourceNotFoundException e) {
+    public ResponseEntity<CartServiceAppError> catchResourceNotFoundException(ResourceNotFoundException e) {
         log.error(e.getMessage(), e);
-        return new ResponseEntity<>(new AppError(HttpStatus.NOT_FOUND.value(), e.getMessage()), HttpStatus.NOT_FOUND);
+        return new ResponseEntity<>(new CartServiceAppError(CartServiceAppError.CartServiceErrors.CART_IS_BROKEN.name(), e.getMessage()), HttpStatus.NOT_FOUND);
+    }
+
+    @ExceptionHandler
+    public ResponseEntity<CartServiceAppError> catchCartIsBrokenException(CartIsBrokenException e) {
+        log.error(e.getMessage(), e);
+        return new ResponseEntity<>(new CartServiceAppError(CartServiceAppError.CartServiceErrors.CART_IS_BROKEN.name(), e.getMessage()), HttpStatus.INTERNAL_SERVER_ERROR);
     }
 }
