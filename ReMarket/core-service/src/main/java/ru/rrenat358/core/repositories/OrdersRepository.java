54beb26e135b12cc4e82c890a0cf96d1facc33c3
rrenat358 <rrenat358@gmail.com>
2023-07-13 17:34:18 +0300

    1732 runnable. переписан запрос sql для topProductsByAllOrders()

--- ReMarket/core-service/src/main/java/ru/rrenat358/core/repositories/OrdersRepository.java
+++ ReMarket/core-service/src/main/java/ru/rrenat358/core/repositories/OrdersRepository.java
@@ -22,6 +22,7 @@ public interface OrdersRepository extends JpaRepository<Order, Long> {
 //    @QueryHints(
 //            @QueryHint(name = "org.hibernate.cacheable", value = "false")
 //    )
+/*
     @Query("""
             SELECT p, COUNT(*)
             FROM Product p
@@ -31,6 +32,30 @@ public interface OrdersRepository extends JpaRepository<Order, Long> {
             ORDER BY COUNT(*) DESC
             """)
     List<Product> topProductsByAllOrders(Pageable pageable);
+*/
+
+/*
+    @Query("""
+            SELECT p, COUNT(*)
+            FROM Product p
+            INNER JOIN OrderItem oi
+            ON p.id = oi.product.id
+            GROUP BY oi.product.id
+            ORDER BY COUNT(*) DESC
+            """)
+    List<Product> topProductsByAllOrders(Pageable pageable);
+*/
+
+    @Query("""
+            SELECT p, COUNT(*)
+            FROM Product p
+            INNER JOIN OrderItem oi
+            ON p.id = oi.product.id
+            GROUP BY p
+            ORDER BY COUNT(*) DESC
+            """)
+    List<Product> topProductsByAllOrders(Pageable pageable);
+
 
 
 
