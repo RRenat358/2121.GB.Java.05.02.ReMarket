6cb94d06b070c541f30659166d3c378bb55f12a9
rrenat358 <rrenat358@gmail.com>
2023-08-30 17:54:44 +0300

    1754

--- ReMarket/stats-service/src/test/java/ru/rrenat358/stats/services/OrdersStatsServiceTest.java
+++ ReMarket/stats-service/src/test/java/ru/rrenat358/stats/services/OrdersStatsServiceTest.java
@@ -12,7 +12,11 @@ import org.mockito.MockitoAnnotations;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.util.Assert;
 import org.springframework.web.reactive.function.client.ClientRequest;
 import org.springframework.web.reactive.function.client.ClientResponse;
@@ -24,12 +28,12 @@ import ru.rrenat358.stats.integrations.CartServiceIntegration;
 import ru.rrenat358.stats.integrations.CoreServiceIntegration;
 
 import java.io.IOException;
+import java.net.ConnectException;
 import java.util.Map;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.*;
 
 @SpringBootTest
 //@ExtendWith(MockitoExtension.class)
@@ -39,116 +43,11 @@ class OrdersStatsServiceTest {
     @Autowired
     private OrdersStatsService ordersStatsService;
 
-    @Mock
-    private CoreServiceIntegration coreServiceIntegrationMock = Mockito.mock(CoreServiceIntegration.class);
-
-    @Mock
-    private WebClient webClientMock = Mockito.mock(WebClient.class);
-
-/*
-    @Mock
-    private WebClient webClientMock;
-
-    @InjectMocks
-    private CoreServiceIntegration coreServiceIntegrationMock = new CoreServiceIntegration(webClientMock);
-
-    public static MockWebServer mockBackEnd;
-
-    @BeforeAll
-    static void setUp() throws IOException {
-        mockBackEnd = new MockWebServer();
-        mockBackEnd.start();
-    }
-
-    @AfterAll
-    static void tearDown() throws IOException {
-        mockBackEnd.shutdown();
-    }
-
-    @BeforeEach
-    void initialize() {
-        String baseUrl = String.format("http://localhost:%s",
-                mockBackEnd.getPort());
-        coreServiceIntegrationMock = new CoreServiceIntegration(
-                (WebClient) webClientMock.get()
-                .uri(baseUrl));
-    }
- */
+    @MockBean
+    private CoreServiceIntegration coreServiceIntegrationMock;
 
 
 
-    private WebClient webClientMock;
-
-//    Map<String, String> result = myHttpService.callService().block();
-
-    @Test
-    void getNumberOfOrdersByCurrentUser_isUsernameExists_not0() {
-        String username = "Ivan";
-        webClientMock = WebClient.builder()
-                .exchangeFunction(clientRequest ->
-                        Mono.just(ClientResponse.create(HttpStatus.OK)
-                                .header("content-type", "application/json")
-                                .body("{ \"key\" : \"value\"}")
-                                .build())
-                ).build();
-        CoreServiceIntegration coreServiceIntegrationMock = new CoreServiceIntegration(webClientMock);
-        Map<String, String> result = coreServiceIntegrationMock.callService().block();
-/*
-        Mockito.when(coreServiceIntegrationMock.getNumberOfOrdersByCurrentUser("Ivan"))
-                .thenReturn(5);
-*/
-        Integer r = coreServiceIntegrationMock.getNumberOfOrdersByCurrentUser(username);
-        Assertions.assertEquals(5,r);
-    }
-
-    @Mock
-    private ExchangeFunction exchangeFunction;
-
-    @BeforeEach
-    void init() {
-        WebClient webClientMock = WebClient.builder()
-                .exchangeFunction(exchangeFunction)
-                .build();
-
-        CoreServiceIntegration coreServiceIntegrationMock = new CoreServiceIntegration(webClientMock);
-    }
-
-    @Test
-    void callService() {
-        when(exchangeFunction.exchange(any(ClientRequest.class)))
-                .thenReturn(buildMockResponse());
-        Map<String, String> result = coreServiceIntegrationMock.getNumberOfOrdersByCurrentUser().block();
-
-        verify(exchangeFunction).exchange(any());
-
-        // Do assertions here
-    }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-/*
-    @Test
-    void getNumberOfOrdersByCurrentUser_isUsernameExists_not0() {
-        String username = "Ivan";
-
-        Mockito.when(coreServiceIntegrationMock.getNumberOfOrdersByCurrentUser("Ivan"))
-                .thenReturn(5);
-        Integer r = coreServiceIntegrationMock.getNumberOfOrdersByCurrentUser(username);
-        Assertions.assertEquals(5,r);
-    }
-*/
 
 
 
@@ -166,6 +65,15 @@ class OrdersStatsServiceTest {
         assertEquals(0,r);
     }
 
+    @Test
+    void getNumberOfOrdersByCurrentUser_isUsernameExists_not0() {
+        String username = "Ivan";
+
+        Mockito.when(coreServiceIntegrationMock.getNumberOfOrdersByCurrentUser("Ivan"))
+                .thenReturn(5);
+        Integer result = coreServiceIntegrationMock.getNumberOfOrdersByCurrentUser(username);
+        Assertions.assertEquals(5,result);
+    }
 
 
 
