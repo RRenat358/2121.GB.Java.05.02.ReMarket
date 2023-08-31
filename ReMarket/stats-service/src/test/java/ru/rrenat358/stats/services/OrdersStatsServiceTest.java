d742d1f8920ce3df8342eacad262f256a207e7e7
rrenat358 <rrenat358@gmail.com>
2023-08-31 17:51:03 +0300

    1750

--- ReMarket/stats-service/src/test/java/ru/rrenat358/stats/services/OrdersStatsServiceTest.java
+++ ReMarket/stats-service/src/test/java/ru/rrenat358/stats/services/OrdersStatsServiceTest.java
@@ -1,14 +1,25 @@
 package ru.rrenat358.stats.services;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.jdk8.Jdk8Module;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import com.fasterxml.jackson.module.paramnames.ParameterNamesModule;
+import org.json.JSONException;
+import org.json.JSONObject;
+import org.junit.Before;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.skyscreamer.jsonassert.JSONAssert;
+import org.skyscreamer.jsonassert.JSONCompareMode;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import ru.rrenat358.api.core.OrderDto;
+import ru.rrenat358.stats.integrations.CartServiceIntegration;
 import ru.rrenat358.stats.integrations.CoreServiceIntegration;
 
 import java.math.BigDecimal;
@@ -27,6 +38,18 @@ class OrdersStatsServiceTest {
     @MockBean
     private CoreServiceIntegration coreServiceIntegrationMock;
 
+    @MockBean
+    private CartServiceIntegration cartServiceIntegrationMock;
+
+/*
+    @Before
+    public void setup() {
+        DatabindCodec.mapper()
+                .registerModule(new ParameterNamesModule())
+                .registerModule(new Jdk8Module())
+                .registerModule(new JavaTimeModule());
+    }
+*/
 
     @Test
     void getNumberOfOrdersByCurrentUser_isUsernameNull_0() {
@@ -61,6 +84,30 @@ class OrdersStatsServiceTest {
         Assertions.assertEquals(expected.getUsername(), actual.get().getUsername());
     }
 
+/*
+    @Test
+    void getAllOrdersByCurrentUser_isUsernameNull_02() throws JSONException, JsonProcessingException {
+        String usernameNull = null;
+//        JSONObject expected = new JSONObject ("{\"id\":0, \"username\":\"username\", \"totalPrice\":0, \"address\":\"0\", \"phone\":\"0\", \"itemList\":null}");
+        JSONObject expected = new JSONObject ("{id:0, username:\"username\", totalPrice:0, address:\"0\", phone:\"0\", itemList:null}");
+        Optional<OrderDto> actual = ordersStatsService.getAllOrdersByCurrentUser(usernameNull);
+
+//        ObjectMapper mapper = new ObjectMapper();
+//        JSONObject actual2 = new JSONObject (mapper.writeValueAsString(actual));
+
+//        Assertions.assertEquals(expected.toString(), actual.toString());
+//        Assertions.assertEquals(expected.toString(), mapper.writeValueAsString(actual));
+//        Assertions.assertEquals(expected.getUsername(), actual.get().getUsername());
+//        JSONAssert.assertEquals(expected, actual,false);
+
+        Optional<OrderDto> data = actual;
+        JsonObject object = Json.parse(input).asObject();
+        String expected2 = "{id:0, username:\"username\", totalPrice:0, address:\"0\", phone:\"0\", itemList:null}";
+        JSONAssert.assertEquals(expected2, String.valueOf(data), false);
+
+    }
+*/
+
     @Test
     void getAllOrdersByCurrentUser_isUsernameEmpty_0() {
         String usernameEmpty = "";
@@ -85,7 +132,22 @@ class OrdersStatsServiceTest {
 
     @Test
     void topProductsByAllCarts() {
+
+        Optional<OrderDto> expected = Optional.ofNullable(new OrderDto(1L, "Ivan", BigDecimal.valueOf(0), "0", "0", null));
+        Mockito.when(coreServiceIntegrationMock.getAllOrdersByCurrentUser("Ivan"))
+                .thenReturn(expected);
+
+
+
+
     }
+
+
+
+
+
+
+
 
     @Test
     void topProductsByAllOrders() {
