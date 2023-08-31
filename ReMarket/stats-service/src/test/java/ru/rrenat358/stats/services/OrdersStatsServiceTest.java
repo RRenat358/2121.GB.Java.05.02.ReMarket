78a7512179f4765c827ab36d289621da6ae307ab
rrenat358 <rrenat358@gmail.com>
2023-08-31 14:44:36 +0300

    1444

--- ReMarket/stats-service/src/test/java/ru/rrenat358/stats/services/OrdersStatsServiceTest.java
+++ ReMarket/stats-service/src/test/java/ru/rrenat358/stats/services/OrdersStatsServiceTest.java
@@ -1,39 +1,20 @@
 package ru.rrenat358.stats.services;
 
-import okhttp3.mockwebserver.MockResponse;
-import okhttp3.mockwebserver.MockWebServer;
-import org.junit.jupiter.api.*;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.junit.jupiter.params.ParameterizedTest;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
 import org.mockito.Mockito;
-import org.mockito.MockitoAnnotations;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpMethod;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
-import org.springframework.util.Assert;
-import org.springframework.web.reactive.function.client.ClientRequest;
-import org.springframework.web.reactive.function.client.ClientResponse;
-import org.springframework.web.reactive.function.client.ExchangeFunction;
-import org.springframework.web.reactive.function.client.WebClient;
-import reactor.core.publisher.Mono;
-import ru.rrenat358.stats.exceptions.CoreServiceIntegrationException;
-import ru.rrenat358.stats.integrations.CartServiceIntegration;
+import ru.rrenat358.api.core.OrderDto;
 import ru.rrenat358.stats.integrations.CoreServiceIntegration;
 
-import java.io.IOException;
-import java.net.ConnectException;
-import java.util.Map;
+import java.math.BigDecimal;
+import java.util.Optional;
 
-import static org.junit.jupiter.api.Assertions.*;
-import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.Mockito.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @SpringBootTest
 //@ExtendWith(MockitoExtension.class)
@@ -47,42 +28,65 @@ class OrdersStatsServiceTest {
     private CoreServiceIntegration coreServiceIntegrationMock;
 
 
-
-
-
-
     @Test
     void getNumberOfOrdersByCurrentUser_isUsernameNull_0() {
         String usernameNull = null;
         Integer r = ordersStatsService.getNumberOfOrdersByCurrentUser(usernameNull);
-        assertEquals(0,r);
+        Assertions.assertEquals(0, r);
     }
 
     @Test
     void getNumberOfOrdersByCurrentUser_isUsernameEmpty_0() {
         String usernameEmpty = "";
         Integer r = ordersStatsService.getNumberOfOrdersByCurrentUser(usernameEmpty);
-        assertEquals(0,r);
+        Assertions.assertEquals(0, r);
     }
 
     @Test
     void getNumberOfOrdersByCurrentUser_isUsernameExists_not0() {
         String username = "Ivan";
-
         Mockito.when(coreServiceIntegrationMock.getNumberOfOrdersByCurrentUser("Ivan"))
                 .thenReturn(5);
         Integer result = coreServiceIntegrationMock.getNumberOfOrdersByCurrentUser(username);
-        Assertions.assertEquals(5,result);
+        Assertions.assertEquals(5, result);
     }
-
-
-
-
 
 
     @Test
     void getAllOrdersByCurrentUser() {
     }
+
+    @Test
+    void getAllOrdersByCurrentUser_isUsernameNull_0() {
+        String usernameNull = null;
+        Optional<OrderDto> actual = ordersStatsService.getAllOrdersByCurrentUser(usernameNull);
+        OrderDto expected = new OrderDto(0L, "[username]", BigDecimal.valueOf(0), "0", "0", null);
+        Assertions.assertEquals(expected.getId(), actual.get().getId());
+        Assertions.assertEquals(expected.getUsername(), actual.get().getUsername());
+    }
+
+
+
+/*
+    @Test
+    void getAllOrdersByCurrentUser_isUsernameEmpty_0() {
+        String usernameEmpty = "";
+        Integer r = ordersStatsService.getAllOrdersByCurrentUser(usernameEmpty);
+        assertEquals(0,r);
+    }
+*/
+
+
+/*
+    @Test
+    void getAllOrdersByCurrentUser_isUsernameExists_0() {
+        String usernameNull = null;
+        Optional<OrderDto> result = ordersStatsService.getAllOrdersByCurrentUser(usernameNull);
+        Optional<OrderDto> expected = new OrderDto(0L, "[username]", BigDecimal.valueOf(0), "0", "0", null);
+        Assertions.assertEquals(expected, result);
+    }
+*/
+
 
     @Test
     void topProductsByAllCarts() {
@@ -91,7 +95,6 @@ class OrdersStatsServiceTest {
     @Test
     void topProductsByAllOrders() {
     }
-
 
 
 }
