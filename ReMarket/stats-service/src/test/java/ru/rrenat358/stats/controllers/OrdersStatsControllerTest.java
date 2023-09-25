d6aa3c21ca982145ae42203786f5ce8789b14bea
rrenat358 <rrenat358@gmail.com>
2023-09-25 22:45:24 +0300

    2245

---
+++ ReMarket/stats-service/src/test/java/ru/rrenat358/stats/controllers/OrdersStatsControllerTest.java
@@ -0,0 +1,24 @@
+package ru.rrenat358.stats.controllers;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class OrdersStatsControllerTest {
+
+    @Test
+    void getNumberOfOrdersByCurrentUser() {
+    }
+
+    @Test
+    void getAllOrdersByCurrentUser() {
+    }
+
+    @Test
+    void topProductsByAllCarts() {
+    }
+
+    @Test
+    void topProductsByAllOrders() {
+    }
+}
