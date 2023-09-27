474cfe1a0664628cb09b032115c4d996480f24aa
rrenat358 <rrenat358@gmail.com>
2023-09-27 23:00:53 +0300

    2300 @Test void getNumberOfOrdersByCurrentUser_isNumberOfOrders()

--- ReMarket/stats-service/src/test/java/ru/rrenat358/stats/controllers/OrdersStatsControllerTest.java
+++ ReMarket/stats-service/src/test/java/ru/rrenat358/stats/controllers/OrdersStatsControllerTest.java
@@ -1,6 +1,5 @@
 package ru.rrenat358.stats.controllers;
 
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,7 +10,6 @@ import org.springframework.test.web.servlet.MockMvc;
 import ru.rrenat358.stats.services.OrdersStatsService;
 
 import static org.hamcrest.core.Is.is;
-import static org.junit.jupiter.api.Assertions.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
@@ -23,58 +21,37 @@ class OrdersStatsControllerTest {
     @Autowired
     private MockMvc mockMvc;
 
-//    @MockBean
-//    private OrdersStatsService ordersStatsService;
-
-    @Autowired
+    @MockBean
     private OrdersStatsService ordersStatsService;
-
 
 /*
     https://www.codejava.net/frameworks/spring-boot/unit-testing-rest-apis-tutorial
     https://www.petrikainulainen.net/programming/spring-framework/unit-testing-of-spring-mvc-controllers-rest-api/
 */
 
+
+    //============================================================
 /*
     @Test
     void getNumberOfOrdersByCurrentUser() {
     }
 */
 
-/*
     @Test
-    void getNumberOfOrdersByCurrentUser() throws Exception {
-        Integer NumberOfOrders = 5;
-
-        mockMvc.perform(get("api/v1/orders-stats/number-of-orders-by-user"))
-                .andDo(print())
-                .andExpect(status().isOk())
-                .andExpect(jsonPath("$").exists());
-
-
-//        Assertions.assertEquals(5, );
-    }
-*/
-
-    @Test
-    void getNumberOfOrdersByCurrentUser() throws Exception {
-
+    void getNumberOfOrdersByCurrentUser_isNumberOfOrders() throws Exception {
         Integer numberOfOrders = 5;
         String username = "Ivan";
 
-        Mockito.when(ordersStatsService.getNumberOfOrdersByCurrentUser(username)).thenReturn(numberOfOrders);
+        Mockito.when(ordersStatsService.getNumberOfOrdersByCurrentUser(username))
+                .thenReturn(numberOfOrders);
 
-        mockMvc.perform(get("api/v1/orders-stats/number-of-orders-by-user"))
+        mockMvc.perform(get("/api/v1/orders-stats/number-of-orders-by-user")
+                        .header("username", username))
                 .andExpect(status().isOk())
                 .andExpect(content().contentType("application/json"))
-                .andExpect(jsonPath( "$",is(5)))
+                .andExpect(jsonPath("$", is(5)))
                 .andDo(print());
-
-
     }
-
-
-
 
 
     @Test
