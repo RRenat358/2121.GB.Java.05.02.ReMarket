4ed57e77a4f6a7e2beda12e107be053300a77c45
rrenat358 <rrenat358@gmail.com>
2023-09-28 17:59:24 +0300

    1759

--- ReMarket/stats-service/src/test/java/ru/rrenat358/stats/controllers/OrdersStatsControllerTest.java
+++ ReMarket/stats-service/src/test/java/ru/rrenat358/stats/controllers/OrdersStatsControllerTest.java
@@ -1,5 +1,6 @@
 package ru.rrenat358.stats.controllers;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -7,7 +8,11 @@ import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMock
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.web.servlet.MockMvc;
+import ru.rrenat358.api.core.OrderDto;
 import ru.rrenat358.stats.services.OrdersStatsService;
+
+import java.math.BigDecimal;
+import java.util.Optional;
 
 import static org.hamcrest.core.Is.is;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
@@ -24,6 +29,8 @@ class OrdersStatsControllerTest {
     @MockBean
     private OrdersStatsService ordersStatsService;
 
+    private static final String BASE_PATH = "/api/v1/orders-stats";
+
 /*
     https://www.codejava.net/frameworks/spring-boot/unit-testing-rest-apis-tutorial
     https://www.petrikainulainen.net/programming/spring-framework/unit-testing-of-spring-mvc-controllers-rest-api/
@@ -31,38 +38,125 @@ class OrdersStatsControllerTest {
 
 
     //============================================================
-/*
     @Test
-    void getNumberOfOrdersByCurrentUser() {
+    void getNumberOfOrdersByCurrentUser_isUsernameNull_0() throws Exception {
+        String username = null;
+
+        Mockito.when(ordersStatsService.getNumberOfOrdersByCurrentUser(username))
+                .thenReturn(0);
+
+        mockMvc.perform(get(BASE_PATH + "/number-of-orders-by-user")
+                        .header("username", ""))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType("application/json"))
+                .andExpect(jsonPath("$", is(0)))
+//                .andDo(print())
+        ;
     }
-*/
 
     @Test
-    void getNumberOfOrdersByCurrentUser_isNumberOfOrders() throws Exception {
+    void getNumberOfOrdersByCurrentUser_isUsernameEmpty_0() throws Exception {
+        String username = "";
+
+        Mockito.when(ordersStatsService.getNumberOfOrdersByCurrentUser(username))
+                .thenReturn(0);
+
+        mockMvc.perform(get(BASE_PATH + "/number-of-orders-by-user")
+                        .header("username", ""))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType("application/json"))
+                .andExpect(jsonPath("$", is(0)))
+                .andDo(print())
+        ;
+    }
+
+    @Test
+    void getNumberOfOrdersByCurrentUser_isUsernameExists_not0() throws Exception {
         Integer numberOfOrders = 5;
         String username = "Ivan";
 
         Mockito.when(ordersStatsService.getNumberOfOrdersByCurrentUser(username))
                 .thenReturn(numberOfOrders);
 
-        mockMvc.perform(get("/api/v1/orders-stats/number-of-orders-by-user")
+        mockMvc.perform(get(BASE_PATH + "/number-of-orders-by-user")
                         .header("username", username))
                 .andExpect(status().isOk())
                 .andExpect(content().contentType("application/json"))
                 .andExpect(jsonPath("$", is(5)))
-                .andDo(print());
+//                .andDo(print())
+        ;
     }
 
 
+
+    //============================================================
+/*
     @Test
     void getAllOrdersByCurrentUser() {
     }
+*/
+    @Test
+    void getAllOrdersByCurrentUser_isUsernameNull_0() throws Exception {
+        String username = null;
+        OrderDto orderDtoList = new OrderDto(0L, "[username]", BigDecimal.valueOf(0), "0", "0", null);
+
+        Mockito.when(ordersStatsService.getAllOrdersByCurrentUser(username))
+                .thenReturn(Optional.of(orderDtoList));
+
+        mockMvc.perform(get(BASE_PATH + "/all-orders")
+                        .header("username", ""))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType("application/json"))
+                .andExpect(jsonPath("$", is(orderDtoList)))
+                .andDo(print())
+        ;
+    }
+
+    @Test
+    void getAllOrdersByCurrentUser_isUsernameNull_0_2() throws Exception {
+        String username = null;
+        OrderDto orderDtoList = new OrderDto(0L, "[username]", BigDecimal.valueOf(0), "0", "0", null);
+
+        Mockito.when(ordersStatsService.getAllOrdersByCurrentUser(username))
+                .thenReturn(Optional.of(orderDtoList));
+
+        mockMvc.perform(get(BASE_PATH + "/all-orders")
+                        .header("username", ""))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType("application/json"))
+                .andExpect(jsonPath("", is(orderDtoList)))
+                .andDo(print())
+        ;
+    }
+
+    @Test
+    void getAllOrdersByCurrentUser_isUsernameEmpty_0() {
+
+
+    }
+
+    @Test
+    void getAllOrdersByCurrentUser_isUsernameExists_not0() {
+
+
+    }
+
+
+    //============================================================
 
     @Test
     void topProductsByAllCarts() {
     }
 
+
+
+    //============================================================
+
     @Test
     void topProductsByAllOrders() {
     }
+
+
+
+
 }
